c8f0bef9cdede2766500810837f807f230b70baa
vinod liyanage <vinodsliyanage@gmail.com>
2023-03-24 12:30:48 +0530

    add types.

---
+++ app/types/types.ts
@@ -0,0 +1,11 @@
+export type SignUpValues = {
+    firstName: string;
+    lastName: string;
+    gender: string;
+    dateOfBirth: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
+    userType: string;
+    bandId: string;
+  };
