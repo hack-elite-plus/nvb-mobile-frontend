5adf634ad90aa2e444ce90cb94fd96a4ed96547e
vinod liyanage <vinodsliyanage@gmail.com>
2023-03-24 12:12:30 +0530

    add enums.

---
+++ app/enums/enums.ts
@@ -0,0 +1,15 @@
+export const enum UserType {
+  PET_OWNER = "PET_OWNER",
+  BICKER = "BICKER",
+  JOGGER = "JOGGER",
+  NONE = "NONE",
+}
+
+export const enum Step {
+  NAME = 1,
+  EMAIL_AND_PASSWORD,
+  GENDER,
+  BAND_ID,
+  USER_TYPE,
+  PET_DETAILS
+}
